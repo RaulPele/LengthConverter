5b18c25d42a9b2980b574ccb1bf085eed7f0f1db
Raul Pele <raul.pele2001@gmail.com>
2022-07-21 12:41:00 +0300

    Initial Commit

---
+++ LengthConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LengthConverter
+//
+//  Created by Raul Pele on 21.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConverterApp.swift
+//  LengthConverter
+//
+//  Created by Raul Pele on 21.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
